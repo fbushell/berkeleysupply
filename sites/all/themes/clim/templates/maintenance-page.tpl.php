93b141fe151252b78c22bdc0fa90a768a6c493cf
fbushell <frank@gocirro.com>
2012-11-27 23:31:09 -0700

    added clim

---
+++ sites/all/themes/clim/templates/maintenance-page.tpl.php
@@ -0,0 +1,15 @@
+<div class="container" style="width: 70%; max-width: 1000px; margin: 70px auto 0 auto; font-family: 'courier new', sans-serif;">
+  <div style="max-width: 338px; margin: 0 auto;"> 
+    <img src="<?php print $logo; ?>" style="max-width: 100%;" alt="<?php print t('Home'); ?>"/>
+  </div>
+  
+  <div style="text-align: center; margin-top: 60px; font-size: 14px; line-height: 1.4em;">
+    We are putting the finishing touches on the new site </br>
+    For news visit our <a href="https://www.facebook.com/BerkeleySupply?fref=ts">facebook</a> page
+    </br></br></br>
+    Talk soon!
+  </div>
+  
+</div>
+
+
