a3a8a9f03eb24715f6ed882f08da39622a0c2233
fbushell <frank@gocirro.com>
2012-11-28 23:18:49 -0700

    added styling with fixed border

--- sites/all/themes/clim/templates/page.tpl.php
+++ sites/all/themes/clim/templates/page.tpl.php
@@ -1,27 +1,21 @@
+<div class="frame">
 <header id="header">
 	<div class="container">
     <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
       <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
     </a>
-    <nav id="main-menu">
-      <?php include 'menu.inc'; ?>
-    </nav>
 	</div>
 </header>
-
-<div class="container" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="row">
-    <?php print $messages; ?>
-    <?php global $user; if ( $user->uid ): ?>
-      <div id="content-header" class="span12">
-      	<div class="tabs"><?php print render($tabs); ?></div>
-      </div> 
-    <?php endif; ?>
-    <div class="span12">
-    	<?php print render($page['content']) ?>
-    </div>
-  </div>
 </div>
+
+<div class="wrapper" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print $messages; ?>
+    <?php print render($page['content']) ?>
+</div>
+<div class="stripe-top"></div>
+<div class="stripe-right"></div>
+<div class="stripe-bottom"></div>
+<div class="stripe-left"></div>
 
 <footer id="footer">
   <div class="container">
